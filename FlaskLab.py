b3ebe8ca2e6933374c62e1b172ecd431e10e6063
Anthony Duenez Ramirez <subchat6@gmail.com>
2025-04-16 20:46:15 -0700

    added css and other routes

--- FlaskLab.py
+++ FlaskLab.py
@@ -6,6 +6,8 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from PIL import Image
 from datetime import datetime
+import smtplib
+from email.message import EmailMessage
 
 
 # create an instance of Flask
@@ -13,7 +15,22 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'csumb-otter'
 bootstrap = Bootstrap5(app)
 
-# route decorator binds a function to a URL
+# home page (about page)
 @app.route('/')
-def index():
+def home():
     return render_template('home.html')
+
+# steam page (lists games)
+@app.route('/steam')
+def steam():
+    return render_template('steam.html')
+
+# fortnite page (lists skins)
+# maybe categorize between pickaxes or skins
+@app.route('/fortnite')
+def fortnite():
+    return render_template('fornite.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
