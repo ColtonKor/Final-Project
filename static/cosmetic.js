656d3f95ac4196150ab492038d1dc676729d583b
ColtonKor <ckorhummel@csumb.edu>
2025-05-03 17:05:41 -0700

    I just remembered some of the values are N/A

--- static/cosmetic.js
+++ static/cosmetic.js
@@ -21,8 +21,8 @@ function openModal(cardElement) {
     modal.querySelector(".modal-description").textContent = description;
     modal.querySelector(".modal-type").textContent = type;
     modal.querySelector(".modal-rarity").textContent = rarity;
-    modal.querySelector(".modal-series").textContent = `Part of the ${series} series`;
-    modal.querySelector(".modal-set").textContent = `Part of the ${set} set`;
+    modal.querySelector(".modal-series").textContent = series;
+    modal.querySelector(".modal-set").textContent = set;
     modal.querySelector(".modal-introduction").textContent = `Release Date: ${introduction}`;
 
     modal.querySelector("input[name='image']").value = image;
